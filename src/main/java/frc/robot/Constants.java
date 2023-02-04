c605426a79faeefa68db4980c4ea51f2e242cf2b
KeaniKina <keani1028@gmail.com>
2023-02-03 17:39:08 -1000

    Added comments

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,11 +4,14 @@ package frc.robot;
 public final class Constants {
 
   public final class DrivetrainConstants{
+
+    // MOTOR PORTS
     public static final int leftFrontPort = 6;
     public static final int leftBackPort = 3;
     public static final int rightFrontPort = 2;
     public static final int rightBackPort = 15;
 
+    // DRIVETRAIN SPEED CONSTANT
     public static final double motorSpeed = 0.1;
   }
 
